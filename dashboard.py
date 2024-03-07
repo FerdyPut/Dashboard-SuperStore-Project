238abf04bde1b1f4512fdda6fc11a2a7c1ab9f82
Ferdyansyah Permana Putra <96618837+FerdyPut@users.noreply.github.com>
2024-03-07 14:51:57 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -26,7 +26,7 @@ if f1 is not None:
     
     # Cek tipe file yang diunggah
     if filename.endswith('.csv'):
-        df = pd.read_csv(f1, sep='\t')
+        df = pd.read_csv(f1, sep='t')
     else:
         df = pd.read_excel(f1)
 
