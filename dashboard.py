2d20663279cdc751dc6330fb52215cd664790195
Ferdyansyah Permana Putra <96618837+FerdyPut@users.noreply.github.com>
2024-03-07 11:47:41 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,9 +1,6 @@
 import streamlit as st
 import plotly.express as px
 import pandas as pd 
-import os
-import warnings
-warnings.filterwarnings('ignore')
 
 
 st.set_page_config(page_title="Dashboard Superstore", page_icon=":bar_chart:", layout="wide")
